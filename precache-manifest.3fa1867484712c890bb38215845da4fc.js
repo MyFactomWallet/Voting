4e44b1f49564c6cafb6a9397cfc02439fb35f1f4
enducker <31051325+enducker@users.noreply.github.com>
2019-12-23 20:34:45 -0500

    Updates

---
+++ precache-manifest.3fa1867484712c890bb38215845da4fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e25c2defd6566b1b4891d3339a9ac71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af32eecbfbe06d82aca9",
+    "url": "/static/css/main.c4777e6c.chunk.css"
+  },
+  {
+    "revision": "32ff4759444a5ae51de2",
+    "url": "/static/js/2.2f0c8545.chunk.js"
+  },
+  {
+    "revision": "af32eecbfbe06d82aca9",
+    "url": "/static/js/main.55b4e3d9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "167d4936ca2790d8b621fba1646afea2",
+    "url": "/static/media/factomProtocolLogo.167d4936.svg"
+  },
+  {
+    "revision": "e79a5c9c8039fc95d090ad40e32ba0fc",
+    "url": "/static/media/headerLogo.e79a5c9c.png"
+  },
+  {
+    "revision": "273726a524bbde360e0b627fe72dd204",
+    "url": "/static/media/ledgerLogo.273726a5.svg"
+  }
+]);
